accacaf4002d53b6d459a1d7ddc0d888ad2203a7
Hussin Saheb <Hsaheb@spartaglobal.com>
2018-09-25 11:27:45 +0100

    made changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,9 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+apt_update 'update_sources' do
+  action :update
+end
 include_recipe("nodejs")
 
 nodejs_npm "pm2"
@@ -14,6 +17,7 @@ end
 
 template '/etc/nginx/sites-available/proxy.conf' do
   source 'proxy.conf.erb'
+  notifies(:restart, 'service[nginx]')
 end
 
 link '/etc/nginx/sites-enabled/proxy.conf' do
@@ -22,9 +26,8 @@ end
 
 link '/etc/nginx/sites-enabled/default' do
   action :delete
+  notifies(:restart, 'service[nginx]')
 end
-
-
 
 service "nginx" do
   action [:enable, :start]
